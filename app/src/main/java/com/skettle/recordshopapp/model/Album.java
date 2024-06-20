42ef7ad93ca9b1f782a60c580eacfb635aa420c9
Sam Kettle <sam.kettle3@icloud.com>
2024-06-20 13:58:59 +0100

    feat: Album model updated to implement two-way binding

--- app/src/main/java/com/skettle/recordshopapp/model/Album.java
+++ app/src/main/java/com/skettle/recordshopapp/model/Album.java
@@ -1,6 +1,10 @@
 package com.skettle.recordshopapp.model;
 
-public class Album {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import com.skettle.recordshopapp.BR;
+
+public class Album extends BaseObservable {
 
     long id;
     String artist;
@@ -21,27 +25,63 @@ public class Album {
         this.stockQuantity = stockQuantity;
     }
 
+    @Bindable
     public long getId() {
         return id;
     }
 
+    @Bindable
     public String getArtist() {
         return artist;
     }
 
+    @Bindable
     public String getGenre() {
         return genre;
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
+    @Bindable
     public int getReleaseYear() {
         return releaseYear;
     }
 
+    @Bindable
     public int getStockQuantity() {
         return stockQuantity;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+        notifyPropertyChanged(BR.artist);
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+        notifyPropertyChanged(BR.genre);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.releaseYear);
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+        notifyPropertyChanged(BR.stockQuantity);
     }
 }
