01f36617f5f012f1e97b9927e26f6d0713eb261c
Sam Kettle <sam.kettle3@icloud.com>
2024-06-20 11:16:03 +0100

    Project initialised

---
+++ app/src/main/java/com/skettle/recordshopapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.skettle.recordshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
