02bb5260477e961fd503ebbc1c74afbf24663015
Sam Kettle <sam.kettle3@icloud.com>
2024-06-21 11:39:20 +0100

    feat: activity_add_new_album.xml basic design created

---
+++ app/src/main/java/com/skettle/recordshopapp/ui/addalbum/AddNewAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.skettle.recordshopapp.ui.addalbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.skettle.recordshopapp.R;
+
+public class AddNewAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_album);
+    }
+}
