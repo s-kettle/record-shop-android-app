783a50d79a52f8892ab525f4571e0a1c5e0b83d2
Sam Kettle <sam.kettle3@icloud.com>
2024-06-21 13:18:07 +0100

    feat: basic add album functionality and UI complete

--- app/src/main/java/com/skettle/recordshopapp/ui/mainactivity/MainActivityClickHandler.java
+++ app/src/main/java/com/skettle/recordshopapp/ui/mainactivity/MainActivityClickHandler.java
@@ -1,4 +1,20 @@
 package com.skettle.recordshopapp.ui.mainactivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import com.skettle.recordshopapp.ui.addalbum.AddNewAlbumActivity;
+
 public class MainActivityClickHandler {
+
+    Context context;
+
+    public MainActivityClickHandler(Context context) {
+        this.context = context;
+    }
+
+    public void addAlbumButtonClicked(View view) {
+        Intent intent = new Intent(view.getContext(), AddNewAlbumActivity.class);
+        context.startActivity(intent);
+    }
 }
