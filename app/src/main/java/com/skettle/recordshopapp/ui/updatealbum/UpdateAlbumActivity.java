8c65be06280fbb1cb3e4584796e8d588eeac077a
Sam Kettle <sam.kettle3@icloud.com>
2024-06-24 12:25:13 +0100

    feat: UpdateAlbumActivity onCreate implemented

--- app/src/main/java/com/skettle/recordshopapp/ui/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/skettle/recordshopapp/ui/updatealbum/UpdateAlbumActivity.java
@@ -2,13 +2,39 @@ package com.skettle.recordshopapp.ui.updatealbum;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
 import com.skettle.recordshopapp.R;
+import com.skettle.recordshopapp.databinding.ActivityUpdateAlbumBinding;
+import com.skettle.recordshopapp.model.Album;
+import com.skettle.recordshopapp.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbumActivity extends AppCompatActivity {
+
+    ActivityUpdateAlbumBinding binding;
+    UpdateAlbumClickHandler clickHandler;
+    MainActivityViewModel model;
+    Album album;
+
+    private static final String ALBUM_KEY = "album";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_album);
+
+        album = getIntent().getParcelableExtra("ALBUM_KEY", Album.class);
+
+        binding.setAlbum(album);
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_update_album
+        );
+
+        clickHandler = new UpdateAlbumClickHandler(
+                this,
+                album,
+                model
+        );
     }
 }
