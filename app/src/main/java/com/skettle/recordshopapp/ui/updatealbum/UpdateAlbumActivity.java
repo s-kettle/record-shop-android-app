b441c7faa570fd50f96372f9abc7883646c22c23
Sam Kettle <sam.kettle3@icloud.com>
2024-06-24 11:06:05 +0100

    feat: add update album activity and layout files

---
+++ app/src/main/java/com/skettle/recordshopapp/ui/updatealbum/UpdateAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.skettle.recordshopapp.ui.updatealbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.skettle.recordshopapp.R;
+
+public class UpdateAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_album);
+    }
+}
